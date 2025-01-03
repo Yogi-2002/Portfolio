eff4e704f1b4ccc802d944ae034a34cd99287146
Yogiswari <yogiswariramesh14@gmail.com>
2025-01-03 14:05:55 +0530

    updated portfolio

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// Simple JavaScript to handle smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
